9a69324ae0ff099e336891b7513dfa4a9abf022a
Phu Chau minh <chauminhphu23@gmail.com>
2025-11-10 18:17:13 +0700

    e

---
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NhomProject.Models;
+using NhomProject.Areas.Admin.Model.ViewModel;
+using PagedList;
+using System.IO;
+
+namespace NhomProject.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Admin/Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Admin/Model/ViewModel/ProductSearchVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NhomProject.Models;
+using PagedList;
+
+namespace NhomProject.Areas.Admin.Model.ViewModel
+{
+    public class ProductSearchVM
+    {
+        
+        public string SearchTerm { get; set; }
+        public decimal? MinPrice { get; set; } 
+        public decimal? MaxPrice { get; set; }
+        public string SortOrder { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; } = 10; 
+        public IPagedList<Products> Products { get; set; }
+    }
+}
